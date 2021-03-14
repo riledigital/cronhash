f9a63be3c5bcd4e03aeb21d2df8115e099a32081
Ri Le <rl2999@columbia.edu>
2021-03-14 16:45:34 -0400

    implement sqlite db and request

--- src/cronhash/cronhash.py
+++ src/cronhash/cronhash.py
@@ -1,4 +1,37 @@
-from bs4 import BeautifulSoup
+#from bs4 import BeautifulSoup
 import requests
 import sqlite3
 import hashlib
+from datetime.datetime import datetime
+import pickle
+
+def initialize_database(file_db):
+	con = sqlite3.connect('data.db')
+	cur = con.cursor()
+	# Create table if not exists
+	cur.execute('''CREATE TABLE IF NOT EXISTS sites
+				(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		url TEXT NOT NULL,
+		last_updated TEXT,
+		hash TEXT,
+		content BLOB
+	)
+	''')
+	return cur
+
+def fetch_url_data(url):
+	response = requests.get(url)
+	hashed = hashlib.sha256()
+	hashed.update(response.text)
+	hash_str = hashed.digest()
+
+	last_updated = datetime.now().isoformat()
+	# last_updated = datetime.now().fromisoformat()
+
+	content = pickle.dumps(response)
+	return (None, url, last_updated, hash_str, content)
+
+if __init__ == '__main__':
+	tup = fetch_url_data('http://example.org')
+	print(tup)
